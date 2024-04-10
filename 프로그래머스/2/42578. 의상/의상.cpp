78db16ccc50abcc467443b8c123625a39cede804
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-04-11 00:32:19 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 3.62 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,23 +1,22 @@
-
 #include <string>
 #include <vector>
 #include <unordered_map>
-// 서로 다른 옷의 조합 수, 하루에 최소 한개
-// type별 가짓수를 계산하고 총 경우의 수를 반환한다.
+
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    unordered_map<string,int> m;
 
+    int size = clothes.size();
+    unordered_map <string, int> map; // 의상 종류, 개수
+    
     for(auto cloth: clothes){
-        m[cloth[1]]++; // cloth[1] = 의상 종류
+        map[cloth[1]] ++;
     }
-
-    // 조합 계산
     int answer = 1;
 
-    for(auto pair: m)
-        answer *= (pair.second + 1);
-
-    return answer -1; // 아무것도 안입은 경우 제외
+    for(auto pair: map)
+        answer *= (pair.second+1);
+    
+    
+    return answer - 1;
 }
