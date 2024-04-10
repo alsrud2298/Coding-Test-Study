1e5248e0a9b3fa596d5b72f56f5ecd09b22d9a37
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-04-11 01:02:36 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,32 +1,31 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    queue<pair<int, int>> q;
+    queue<pair<int,int>> q;
     int size = priorities.size();
     
-    for(int i = 0; i < size; i ++){
-        q.push({i,priorities[i]}); //인덱스와 우선순위 저장
-    }
-    sort(priorities.begin(),priorities.end(),greater<>()); // 우선순위 큰 순서대로 정렬
-    
+    for(int i = 0; i < size; i++)
+       q.push({i,priorities[i]});
+
+    sort(priorities.begin(),priorities.end(),greater<>()); // 우선순위 높은 순서대로 정렬
     int i = 0;
     while(!q.empty()){
-        pair<int, int> process = q.front();
+        pair<int,int> procoess = q.front();
         q.pop();
-        
-        if(process.second == priorities[i]){ // 남은 프로세스 중 우선순위가 가장 높으면
-            i++; answer++; //해당 프로세스 실행 & i++, answer++; 
-            if(process.first == location) // 해당 프로세스의 인덱스가 타겟값과 같으면
-                break; // 멈추기
-        }
-        else
-            q.push(process); // 더 높은 우선순위가 있으면 다시 큐에 넣기
+        if(procoess.second == priorities[i]){
+            answer++; 
+            i++;
+
+            if(procoess.first == location)
+                break;
+        }else
+            q.push(procoess);
     }
     return answer;
 }
