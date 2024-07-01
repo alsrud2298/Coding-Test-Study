de380f58062f9b273dc86731d04be9401a8dbfb5
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-07-02 00:01:52 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,12 +1,7 @@
 import sys
 
 n = int(sys.stdin.readline())
-nums = map(int,sys.stdin.readline().split())
+nums = list(map(int,sys.stdin.readline().split()))
 target = int(sys.stdin.readline())
-cnt = 0
-for i in nums:
-    if i == target:
-        cnt +=1
-        
-print(cnt)
-        
+
+print(nums.count(target))
