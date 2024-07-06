9f06a15ad87715880000201d4633f6aa3e6d407a
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-07-07 00:05:27 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,6 @@
+S = input()
+alpha = ['a','b','c','d','e','f','g','h','i','j','k','l',
+        'm','n','o','p','q','r','s','t','u','v','w','x',
+        'y','z']
+for a in alpha:
+    print(S.find(a),end=" ")
