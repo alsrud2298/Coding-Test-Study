ac5162620835cafca9c629dab7e592a366dee333
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-07-23 17:51:45 +0900

    [Bronze II] Title: 소수, Time: 576 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -2,14 +2,16 @@
 M = int(input())
 N = int(input())
 prime = []
-for i in range(M,N+1):
-    factors = []
-    for j in range(1, i+1):
-        if i % j == 0:
-            factors.append(j)
-    if len(factors) == 2:
-        prime.append(i)
-
+for num in range(M,N+1):
+    no_prime = 0
+    if num > 1 :
+        for i in range(2,num):
+            if num % i == 0:
+                no_prime += 1 # 소수가 아닌 경우
+                break
+        if no_prime == 0: # 소수인 경우
+            prime.append(num)
+                
 if len(prime) == 0 :
     print(-1)
 else:
