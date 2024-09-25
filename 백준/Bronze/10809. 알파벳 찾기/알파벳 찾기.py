fd2d2e4191d76df7ed51b9f6ac0e92e26bfb2f26
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-09-25 16:18:25 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,6 +1,12 @@
-S = input()
+# 알파벳이 포함된 경우, 처음 등장 위치 아니라면 -1 출력
+
+word = input()
 alpha = ['a','b','c','d','e','f','g','h','i','j','k','l',
-        'm','n','o','p','q','r','s','t','u','v','w','x',
-        'y','z']
+        'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+answer = []
+
 for a in alpha:
-    print(S.find(a),end=" ")
+    answer.append(word.find(a))
+
+print(*answer)
