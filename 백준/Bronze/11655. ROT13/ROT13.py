49c710ef6bb5111e334ac5bc8c2c425fbf59013f
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-12-18 21:05:22 +0900

    [Bronze I] Title: ROT13, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.py
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -1,24 +1,19 @@
 S = input()
 answer = ""
-lower = []
-upper = []
-for i in range(ord('A'),ord('Z')+1):
-    upper.append(chr(i))
-for i in range(ord('a'),ord('z')+1):
-    lower.append(chr(i))
     
-for i in range(len(S)):
-    if S[i].islower():
-        if ord(S[i]) + 13 < 26:
-            answer += lower[lower.index(S[i])+13]
+for i in S:
+    if i.islower():
+        if ord('a') <= ord(i) + 13 <= ord('z'):
+            answer += chr(ord(i) + 13)
         else:
-            answer += lower[lower.index(S[i])+13-26]
-    elif S[i].isupper():
-        if ord(S[i]) + 13 < 26:
-            answer += upper[upper.index(S[i])+13]
+            answer += chr(ord(i) - 13)
+    elif i.isupper():
+        if ord('A') <= ord(i) + 13 <= ord('Z'):
+            answer += chr(ord(i) + 13)
         else:
-            answer += upper[upper.index(S[i])+13-26]
+            answer += chr(ord(i) - 13)
+
     else:
-        answer += S[i]
+            answer += i
 
 print(answer)
