b10647e89006702537c4e73e0ae852d502adfaf1
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2025-03-14 17:00:52 +0900

    [level 3] Title: 네트워크, Time: 0.89 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -8,10 +8,9 @@ def solution(n, computers):
             if connect != com and computers[com][connect] == 1:
                 if visited[connect] == False:
                     dfs(n, computers, connect, visited)
-    
+                    
     for com in range(n):
         if visited[com] == False:
             dfs(n, computers, com, visited)
-            answer += 1
-    
+            answer += 1 
     return answer
