c2e90095c4c893932242970eb08deb77e0ee140c
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2025-06-25 22:05:43 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,18 @@
+-- 1. 2022년 10월 5일에 등록 CREATED_DATE
+-- 2. 거래상태 SALE -> 판매중, RESERVED -> 예약중, DONE -> 거래완료
+
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    CASE    
+        WHEN STATUS = 'SALE' THEN '판매중'
+        WHEN STATUS = 'RESERVED' THEN '예약중'
+        WHEN STATUS = 'DONE' THEN '거래완료'
+    END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE
+    CREATED_DATE = '2022-10-05'
+ORDER BY
+    BOARD_ID DESC
